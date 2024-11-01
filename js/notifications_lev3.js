d1e1b7c7955f2c11b6deca88d0beba197e5cdf48
Sofia123-ui <sofiabilak59@gmail.com>
2024-11-01 17:56:51 +0200

    Update notifications_lev3.js

--- js/notifications_lev3.js
+++ js/notifications_lev3.js
@@ -1,7 +1,20 @@
+const backgroundAudio = document.getElementById('background-audio');
+const winAudio = document.getElementById('win-audio');
+
 // Показ модального вікна перемоги
 function showWinModal() {
     const winModal = document.querySelector('.modal-win');
     winModal.style.display = 'flex';
+
+    // Зупинка фонової музики
+    backgroundAudio.pause();
+
+    winAudio.currentTime = 0;
+    winAudio.play();
+    setTimeout(() => {
+        winAudio.pause();
+        winAudio.currentTime = 0;
+    }, 16000);
 
     const backToMainButton = winModal.querySelector('.back-to-main');
     backToMainButton.addEventListener('click', showWarningModal);
@@ -31,14 +44,25 @@ function showWarningModal() {
 // Показ модального вікна закінчення часу
 function showTimeOutModal() {
     const timeoutModal = document.querySelector('.modal-timeout');
+    const timeoutAudio = document.getElementById('time-out-audio');
+
+    // Відображаємо модальне вікно
     timeoutModal.style.display = 'flex';
 
+    // Відтворюємо звук вичерпання часу
+    timeoutAudio.currentTime = 0; // Почати з початку звуку
+    timeoutAudio.play();
+
+    // Кнопка для повтору гри
     const retryButton = timeoutModal.querySelector('.retry-game');
     retryButton.addEventListener('click', () => {
         timeoutModal.style.display = 'none';
-        resetGame();
+        timeoutAudio.pause(); // Зупиняємо звук
+        timeoutAudio.currentTime = 0; // Повертаємо на початок
+        resetGame(); // Скидання гри
     });
 
+    // Кнопка для повернення на головну сторінку
     const backToMainButton = timeoutModal.querySelector('.back-to-main-timeout');
     backToMainButton.addEventListener('click', () => {
         window.location.href = 'index.html';
